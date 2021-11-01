fd125f428a596b8e4e401f8234387196363d96ee
Adv <tiffanydpro923@gmail.com>
2021-11-01 17:35:27 +0100

    maj des commentaires dans les fichiers js modal7 et functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,112 +1,112 @@
 //FUNCTIONS FOR VALIDATION FORM
-function isEmpty(input) {
-    if (input === "") {
-      return false
+function isEmpty(input) { //creation de la function isEmpty
+    if (input === "") { // si input is empty
+      return false //retourne false
     }
   }
   
-  function minTwoChar(input) {
-    if (input.length < 2) {
-      return false
+  function minTwoChar(input) { //creation de la function minTwoChar
+    if (input.length < 2) { // si input is <2
+      return false //retourne false
       }
   }
   
-  function checkFirstName(input) {
-    if (minTwoChar(input) === false) {
-      divPrenom.setAttribute('data-error', 'Merci d\'écrire 2 caractères minimum.');
-      divPrenom.setAttribute('data-error-visible', 'true');
-    } else {
-      divPrenom.setAttribute('data-error-visible', 'false');
-      return true;
+  function checkFirstName(input) { //creation de la function checkFirstName
+    if (minTwoChar(input) === false) { // si function minTwoChar appliquée à l'input retourne strictement false
+      divPrenom.setAttribute('data-error', 'Merci d\'écrire 2 caractères minimum.');//afficher contenu du msg d'erreur
+      divPrenom.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
+    } else { //sinon
+      divPrenom.setAttribute('data-error-visible', 'false');//cacher msg d'erreur
+      return true;//retourne true
     }
   }
   
-  function checkLastName(input) {
-    if (minTwoChar(input) === false) {
-      divNom.setAttribute('data-error', 'Merci d\'écrire 2 caractères minimum.');
-      divNom.setAttribute('data-error-visible', 'true');
-    } else {
-      divNom.setAttribute('data-error-visible', 'false');
-      return true;
+  function checkLastName(input) { //creation de la function checkLastName
+    if (minTwoChar(input) === false) {// si function minTwoChar appliquée à l'input retourne strictement false
+      divNom.setAttribute('data-error', 'Merci d\'écrire 2 caractères minimum.');//afficher contenu du msg d'erreur
+      divNom.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
+    } else {//sinon
+      divNom.setAttribute('data-error-visible', 'false');//cacher msg d'erreur
+      return true;//retourne true
     }
   }
   
-  function checkEmail(input) {
-    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
-      divEmail.setAttribute('data-error', 'Merci d\'écrire une adresse email valide.');
-      divEmail.setAttribute('data-error-visible', 'true');
-    } else {
-      divEmail.setAttribute('data-error-visible', 'false');
-      return true;
+  function checkEmail(input) { //creation de la function checkEmail
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) { // si l'input est pas au bon format regex attendu pour emails
+      divEmail.setAttribute('data-error', 'Merci d\'écrire une adresse email valide.');//afficher contenu du msg d'erreur
+      divEmail.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
+    } else {//sinon
+      divEmail.setAttribute('data-error-visible', 'false');//cacher msg d'erreur
+      return true;//retourne true
     }
   }
   
-  function checkBirthdate(input) {
+  function checkBirthdate(input) { //creation de la function checkBirthdate
   
-    let today = new Date();
-    let year = today.getFullYear();
+    let today = new Date(); // initialisation variable today avec new date de la function Date
+    let year = today.getFullYear(); // initialisation variable year qui récupère l'année complète dans la variable today
   
-    if(isEmpty(input) === false) {
-      divBirthdate.setAttribute('data-error', 'Vous devez entrer une date de naissance valide.');
-      divBirthdate.setAttribute('data-error-visible', 'true');
+    if(isEmpty(input) === false) { //si la function isEmpty appliquée à l'input retour un strictement false
+      divBirthdate.setAttribute('data-error', 'Vous devez entrer une date de naissance valide.');//afficher contenu du msg d'erreur
+      divBirthdate.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
     } 
-    else if (input.slice(0, 4) > year - 16) {
-      divBirthdate.setAttribute('data-error', 'Vous devez avoir plus de 16 ans.');
-      divBirthdate.setAttribute('data-error-visible', 'true');
+    else if (input.slice(0, 4) > year - 16) {//sinon si la methode slice (extraire une partie d'un string, ici les 4 premiers caractères en partant de 0) qui prend l'année est supérieure à l'année en cours - 16 ans alors :
+      divBirthdate.setAttribute('data-error', 'Vous devez avoir plus de 16 ans.');//afficher contenu du msg d'erreur
+      divBirthdate.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
     }
-    else if (input.slice(0, 4) < year - 200) {
-      divBirthdate.setAttribute('data-error', 'Plus de 200 ans, sérieusement ?');
-      divBirthdate.setAttribute('data-error-visible', 'true');
+    else if (input.slice(0, 4) < year - 200) {//sinon si la methode slice (extraire une partie d'un string, ici les 4 premiers caractères en partant de 0) qui prend l'année est inférieure à l'année en cours - 16 ans alors :
+      divBirthdate.setAttribute('data-error', 'Plus de 200 ans, sérieusement ?');//afficher contenu du msg d'erreur
+      divBirthdate.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
     }
-      else {
-      divBirthdate.setAttribute('data-error-visible', 'false');
-      return true;
+      else {//sinon
+      divBirthdate.setAttribute('data-error-visible', 'false');//cacher msg d'erreur
+      return true;//retourne true
     }
   }
   
-  function checkQuantityTournament(input) {
-      if (isEmpty(input) === false) {
-        divTournament.setAttribute('data-error', 'Merci de donner un nombre entre 1 et 99.');
-        divTournament.setAttribute('data-error-visible', 'true');
-        } else {
-            divTournament.setAttribute('data-error-visible', 'false');
-          return true;
+  function checkQuantityTournament(input) {//creation de la function checkQuantityTournament
+      if (isEmpty(input) === false) {//si la function isEmpty appliqée à l'input retourne strictement false
+        divTournament.setAttribute('data-error', 'Merci de donner un nombre entre 1 et 99.');//afficher contenu du msg d'erreur
+        divTournament.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
+        } else {//sinon
+            divTournament.setAttribute('data-error-visible', 'false');//cacher msg d'erreur
+          return true;//retourne true
         }
   }
   
-  function checkRadio() {
-        for (let i = 0; i < allRadio.length; i++) {
-          if(!allRadio[i].checked) {
-            divCheckboxLoc.setAttribute('data-error', 'Merci de choisir une ville.');
-            divCheckboxLoc.setAttribute('data-error-visible', 'true');
-          } else {
-            divCheckboxLoc.setAttribute('data-error-visible', 'false');
-          return true;
+  function checkRadio() {//creation de la function checkRadio
+        for (let i = 0; i < allRadio.length; i++) { //pour toutes les données contenues dans allRadio
+          if(!allRadio[i].checked) { // si il n'y a pas une donnée de checked alors:
+            divCheckboxLoc.setAttribute('data-error', 'Merci de choisir une ville.');//afficher contenu du msg d'erreur
+            divCheckboxLoc.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
+          } else {//sinon
+            divCheckboxLoc.setAttribute('data-error-visible', 'false');//cacher msg d'erreur
+          return true;//retourne true
         }
       }
   }
   
-  function checkCheckbox() {
-      if(!generalCondition.checked) {
-        divCheckbox1.setAttribute('data-error', 'Merci de lire et d\'accepter les conditions d\'utilisation.');
-        divCheckbox1.setAttribute('data-error-visible', 'true');
-        } else {
-          divCheckbox1.setAttribute('data-error-visible', 'false');
-          return true;
+  function checkCheckbox() {//creation de la function checkCheckbox
+      if(!generalCondition.checked) {//si generalCondition n'est pas cochée alors
+        divCheckbox1.setAttribute('data-error', 'Merci de lire et d\'accepter les conditions d\'utilisation.');//afficher contenu du msg d'erreur
+        divCheckbox1.setAttribute('data-error-visible', 'true');//afficher msg d'erreur
+        } else {//sinon
+          divCheckbox1.setAttribute('data-error-visible', 'false');//cacher msg d'erreur
+          return true;//retourne true
         }
   }
   
-  function formEnd() {
-    btnSubmit.style.display = "none";
+  function formEnd() { // creation function formEnd
+    btnSubmit.style.display = "none"; // enlever l'affichage du bouton submit sur le message de thanks
     
     //btnSubmit.addEventListener('click', closeModal);
     
     // delete content form 
-    for(let i = 0; i < formData.length; i++) {
-      formData[i].classList.replace("formData", "formDelete");
+    for(let i = 0; i < formData.length; i++) { // pour toutes les données contenues dans formData
+      formData[i].classList.replace("formData", "formDelete"); //replacer formData par formDelete pour supprimer
     }
   
-    // add text thanks and adjust height for text
+    // ajouter text thanks et ajuster height pour text
     form.classList.add("form");
   
     form.style.height = `${formHeight}`;
--- assets/js/modal7.js
+++ assets/js/modal7.js
@@ -10,33 +10,33 @@ function editNav() {
   ////////////////////////////////////////////////////////////////////////////////////////
 
   // DOM Elements
-  const modalbg = document.querySelector(".bground");
-  const btnModalClose = document.querySelector(".close");
-  const modalBtn = document.querySelectorAll(".modal-btn");
+  const modalbg = document.querySelector(".bground"); // sélectionne toute ce qui concerne la class bground dans une variable const
+  const btnModalClose = document.querySelector(".close"); // sélectionne toute ce qui concerne la class close dans une variable const
+  const modalBtn = document.querySelectorAll(".modal-btn"); // sélectionne toute ce qui concerne la class modal-btn dans une variable const
   
   //DOM Elements for form 
-  const form = document.querySelector("form");
-  const firstName = document.querySelector("#first");
-  const lastName = document.querySelector("#last");
-  const email = document.querySelector("#email");
-  const birthdate = document.querySelector('#birthdate');
-  const quantityTournament = document.querySelector("#quantity");
-  const allRadio = document.querySelectorAll('input[name="location"]');
-  const generalCondition = document.querySelector("#checkbox1");
+  const form = document.querySelector("form");// sélectionne toute ce qui concerne le form dans une variable const
+  const firstName = document.querySelector("#first");//sélectionne l'ID first dans une variable const
+  const lastName = document.querySelector("#last");//sélectionne l'ID last dans une variable const
+  const email = document.querySelector("#email");//sélectionne l'ID email dans une variable const
+  const birthdate = document.querySelector('#birthdate');//sélectionne l'ID birthdate dans une variable const
+  const quantityTournament = document.querySelector("#quantity");//sélectionne l'ID quantity dans une variable const
+  const allRadio = document.querySelectorAll('input[name="location"]');//sélectionne les input avec le name location dans une variable const
+  const generalCondition = document.querySelector("#checkbox1");//sélectionne l'ID checkbox1 dans une variable const
    
   // DOM Div Elements use for form error
-  const divPrenom = document.querySelector('#divPrenom');
-  const divNom = document.querySelector('#divNom');
-  const divEmail = document.querySelector('#divEmail');
-  const divBirthdate = document.querySelector('#divBirthdate')
-  const divTournament = document.querySelector('#divTournament');
-  const divCheckboxLoc = document.querySelector('#divLocation');
-  const divCheckbox1 = document.querySelector('#divCheckbox1');
+  const divPrenom = document.querySelector('#divPrenom');//sélectionne l'ID divPrenom dans une variable const
+  const divNom = document.querySelector('#divNom');//sélectionne l'ID divNom dans une variable const
+  const divEmail = document.querySelector('#divEmail');//sélectionne l'ID divEmail dans une variable const
+  const divBirthdate = document.querySelector('#divBirthdate')//sélectionne l'ID divBirthdate dans une variable const
+  const divTournament = document.querySelector('#divTournament');//sélectionne l'ID divTournament dans une variable const
+  const divCheckboxLoc = document.querySelector('#divLocation');//sélectionne l'ID divLocation dans une variable const
+  const divCheckbox1 = document.querySelector('#divCheckbox1');//sélectionne l'ID divCheckbox1 dans une variable const
 
   // DOM Elements use for form validate
-  const btnSubmit = document.querySelector(".btn-submit");
-  const formData = document.querySelectorAll(".formData");
-  const modalEnd = document.querySelector(".modal-end");
+  const btnSubmit = document.querySelector(".btn-submit");// sélectionne toute ce qui concerne la class btn-submit dans une variable const
+  const formData = document.querySelectorAll(".formData");// sélectionne toute ce qui concerne la class formData dans une variable const
+  const modalEnd = document.querySelector(".modal-end");// sélectionne toute ce qui concerne la class modal-end dans une variable const
 
   ////////////////////////////////////////////////////////////////////////////
 
@@ -45,7 +45,6 @@ function editNav() {
   
   //close modal event
   btnModalClose.addEventListener('click', closeModal);
-  
   let formHeight = '';
   
   // launch modal form
@@ -76,62 +75,62 @@ function editNav() {
   //////////////////////////////////////////////////////////////////////////////
   
   // verify firstName
-  firstName.addEventListener("blur", (e) => {
-    checkFirstName(e.target.value)
+  firstName.addEventListener("blur", (e) => { //quand firstName perd son focus, lancer la function checkFirstName sur la value
+    checkFirstName(e.target.value) //applique la fucntion à la value de la cible lorsque l'event "lost focus" se produit
   });  
   
   // verify lastName
-  lastName.addEventListener("blur", (e) => {
-    checkLastName(e.target.value)
+  lastName.addEventListener("blur", (e) => { //quand lastName perd son focus, lancer la function checkLastName sur la value
+    checkLastName(e.target.value) //applique la fucntion à la value de la cible lorsque l'event "lost focus" se produit
   });
   
   // verify email
-  email.addEventListener("blur", (e) => {
-   checkEmail(e.target.value)
+  email.addEventListener("blur", (e) => { //quand email perd son focus, lancer la function checkEmail sur la value
+   checkEmail(e.target.value) //applique la fucntion à la value de la cible lorsque l'event "lost focus" se produit
   });
   
   //verify birthdate
-  birthdate.addEventListener("blur", (e) => {
-    console.log(e)
-    checkBirthdate(e.target.value)
+  birthdate.addEventListener("blur", (e) => { //quand birthdate perd son focus, lancer la function checkBirthdate sur la value
+    console.log(e) // affiche event
+    checkBirthdate(e.target.value) //applique la fucntion à la value de la cible lorsque l'event "lost focus" se produit
   })
   
   //block operator in input quantityTournament
-  quantityTournament.addEventListener("keydown", function(e) {
+  quantityTournament.addEventListener("keydown", function(e) { //quand touche pressée, déclanche la function qui crée une const avec les caractères invalides
     const invalidChars = [
     "-",
     "+",
     "e",
   ];
   
-    if (invalidChars.includes(e.key)) {
-      e.preventDefault();
+    if (invalidChars.includes(e.key)) { // si le champ comporte des caractères invalides, stoper envoi form et lancer msg d'erreur
+      e.preventDefault(); // contient liste des messages d'erreur
     }
   });
   
   // verify quantity tournament 
-  quantityTournament.addEventListener("blur", (e) => {
-    checkQuantityTournament(e.target.value)
+  quantityTournament.addEventListener("blur", (e) => {//quand quantityTournament perd son focus, lancer la function checkQuantityTournament sur la value
+    checkQuantityTournament(e.target.value) //applique la fucntion à la value de la cible lorsque l'event "lost focus" se produit
   });
 
   ////////////////////////////////////////////////////////////////////////////////
   
   // no page reloading
-  form.addEventListener('submit', (e) => {
+  form.addEventListener('submit', (e) => { // quand le form est submit, lancer la fucntino preventDefault à l'event
     e.preventDefault();
   })
   
   // Validate form
-  function validate() {
+  function validate() { // function appelée "onsubmit" pour valider les données encore une fois à l'envoi du form
   
-    if(checkFirstName(firstName.value) &&
-     checkLastName(lastName.value) &&
-      checkEmail(email.value) &&
-       checkBirthdate(birthdate.value) &&
-        checkQuantityTournament(quantityTournament.value) &&
-         checkRadio() &&
-          checkCheckbox() === true ) {
-            formEnd()
+    if(checkFirstName(firstName.value) && //applique la function checkFirstName à la value dans l'input/const first
+     checkLastName(lastName.value) && //applique la function checkLastName à la value dans l'input/const last
+      checkEmail(email.value) && //applique la function checkEmail à la value dans l'input/const email
+       checkBirthdate(birthdate.value) && // applique la function checkBirthdate à la value dans l'input/const birthdate
+        checkQuantityTournament(quantityTournament.value) &&// applique la function checkQuantityTournament à la value dans l'input/const quantityTournament
+         checkRadio() && // lance la function checkRadio 
+          checkCheckbox() === true ) { //vérifie que le résultat de la functino checkCheckbox est strictement égal à true
+            formEnd() //lance la function formEnd si toutes les contraintes précédentes sont respected
           }
     //  to show all the errors at the same time
     // else {
